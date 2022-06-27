82d0323ffbb5c12953dc06434d97b17eb1b931ac
Guilherme Dias Simoes <gds.simoes@gmail.com>
2022-06-27 12:51:56 -0400

    Add buttons functionality

---
+++ index.js
@@ -0,0 +1,21 @@
+const home = document.querySelector("#home .score");
+const guest = document.querySelector("#guest .score");
+
+function changeCounter(n, counter) {
+    const curr = Number(counter.textContent);
+    counter.textContent = curr + n;
+    highlight();
+}
+
+function highlight() {
+    if (Number(home.textContent) > Number(guest.textContent)) {
+        home.classList.toggle("leader", true);
+        guest.classList.toggle("leader", false);
+    } else if (Number(home.textContent) < Number(guest.textContent)) {
+        home.classList.toggle("leader", false);
+        guest.classList.toggle("leader", true);
+    } else {
+        home.classList.toggle("leader", false);
+        guest.classList.toggle("leader", false);
+    }
+}
